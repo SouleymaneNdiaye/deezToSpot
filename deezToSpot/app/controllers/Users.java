f2c2455215faaaf701c40f87d7817cf8bdeec6ea
Souleymane Ndiaye <souleymane.ndiaye@trsb.net>
2016-12-09 18:29:58 +0100

    conf play

---
+++ deezToSpot/app/controllers/Users.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import models.User;
+
+/**
+ * Created by sndiaye on 09/12/2016.
+ */
+public class Users {
+    public User user;
+    public void setId(Long id){
+        user.id=id;
+    }
+}
---
+++ deezToSpot/app/models/User.java
@@ -0,0 +1,9 @@
+package models;
+
+/**
+ * Created by sndiaye on 09/12/2016.
+ */
+public class User {
+    public Long id;
+
+}
