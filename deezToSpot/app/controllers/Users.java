a3305e995b6d6f08a92c76c83556aae2d0af8e6c
Souleymane Ndiaye <souleymane.ndiaye@trsb.net>
2016-12-20 17:53:52 +0100

    Début de projet partie deezer
    Il manque oAuth et la recherche de tous les albums d'un utilisateur

--- deezToSpot/app/controllers/Users.java
+++ deezToSpot/app/controllers/Users.java
@@ -1,13 +1,13 @@
 package controllers;
 
 import models.User;
+import play.mvc.Controller;
+import play.mvc.Util;
 
 /**
  * Created by sndiaye on 09/12/2016.
  */
-public class Users {
-    public User user;
-    public void setId(Long id){
-        user.id=id;
-    }
+public class Users extends Controller{
+
+
 }
--- deezToSpot/app/models/User.java
+++ deezToSpot/app/models/User.java
@@ -1,9 +1,30 @@
 package models;
 
+import play.db.jpa.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by sndiaye on 09/12/2016.
  */
-public class User {
-    public Long id;
+@Entity
+@Table(name="user")
+public class User extends Model {
+
+    @Column(name="user_id")
+    public Long userId;
+
+    @Column(name="nickname")
+    public String nickname;
+
+    public User(Long id, String nickname){
+        this.userId=id;
+        this.nickname=nickname;
+
+    }
+
 
 }
