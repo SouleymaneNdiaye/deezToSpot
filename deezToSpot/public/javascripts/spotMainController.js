a6941f5ac8fea560a8f5764aa9ce3a7c663b5f1b
Unknown <ndiaye.souleymane@live.fr>
2018-02-02 09:20:47 +0100

    feat(spotview)

--- deezToSpot/public/javascripts/spotMainController.js
+++ deezToSpot/public/javascripts/spotMainController.js
@@ -1,28 +1,91 @@
 /**
  * Created by Souleymane on 04/11/2017.
  */
-angular
-    .module('deezToSpot', ['spotify'])
-    .config(function (SpotifyProvider) {
-        SpotifyProvider.setClientId('2c7ac924a805414aac4cc62d009ffaa0');
-        SpotifyProvider.setRedirectUri('http://localhost:9000/channel');
-        SpotifyProvider.setScope('playlist-read-private');
-    })
-    .controller('MainController', ['$scope', 'Spotify', function ($scope, Spotify) {
+var deezToSpot = angular.module('deezToSpot', []);
+deezToSpot.controller("deezToSpot",['$scope',function($scope,$http) {
 
-        $scope.searchArtist = function () {
-            Spotify.search($scope.searchartist, 'artist').then(function (data) {
-                $scope.artists = data.artists.items;
+        $scope.userId=undefined;
+        $scope.userTest=undefined;
+        $scope.user={
+
+            display_name: undefined,
+            email: undefined,
+            external_urls:
+                {spotify: undefined},
+            followers: {href: undefined, total: undefined},
+            href: undefined,
+            type:undefined,
+            uri: undefined
+        };
+
+        function login(callback) {
+
+   var CLIENT_ID = '0ad02084b78a4e379a6b935c898a0849';
+    var REDIRECT_URI = 'http://localhost:9000/viewSpot/';
+
+    function getLoginURL(scopes) {
+        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+            '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+            '&scope=' + encodeURIComponent(scopes.join(' ')) +
+            '&response_type=token';
+    }
+
+    var url = getLoginURL([
+        'user-read-email'
+    ]);
+
+    var width = 450,
+        height = 730,
+        left = (screen.width / 2) - (width / 2),
+        top = (screen.height / 2) - (height / 2);
+
+    window.addEventListener("message", function (event) {
+        var hash = JSON.parse(event.data);
+        if (hash.type == 'access_token') {
+            callback(hash.access_token);
+        }
+    }, false);
+
+    var w = window.open(url);
+
+}
+
+function getUserData(accessToken) {
+    return $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+
+    });
+}
+
+    loginButton = document.getElementById('btn-login');
+
+loginButton.addEventListener('click', function () {
+    login(function (accessToken) {
+        getUserData(accessToken)
+            .then(function (response) {
+                loginButton.style.display = 'none';
             });
-        };
+    });
+});
+        getUser = document.getElementById('get_user');
 
-        $scope.login = function () {
-            console.log("yes t la")
-            Spotify.login().then(function (data) {
-                console.log(data);
-                alert("You are now logged in");
-            }, function () {
-                console.log('didn\'t log in');
-            })
-        };
-    }]);
+        getUser.addEventListener('click', function () {
+    var pathArray=window.location.href.split('=');
+    var accessToken=pathArray[1].split('&')[0];
+
+            getUserData(accessToken)
+                .then(function (response) {
+                    loginButton.style.display = 'none';
+                    $scope.user=$.parseJSON(JSON.stringify(response));
+                    $scope.userTest=$.parseJSON(JSON.stringify(response));
+                    $scope.userId=response.id;
+                    console.log($scope.user);
+                });
+
+    });
+
+
+}]);
